184ef48de2518c14dbc64fd09691c398f7fd3264
Emilia Dunfelt <emilia@dunfelt.se>
2019-12-13 19:59:33 +0100

    Multiplication program commit
    Mult.asm file and Mult.hack file

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,46 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// How it works:	i = R1
+// 			while i > 0 
+//   		    	    R2 += R0
+//   		    	    i -= 1
+// 			return R2
+
+    @R1
+    D = M		// load R1 into D
+
+    @i			// set the backwards counter to R1
+    M = D
+
+    @product		// product = 0 before program begins
+    M = 0
+
+(LOOP)			// while i > 0, keep looping
+    @i
+    D = M
+
+    @END		// otherwise, goto END
+    D;JEQ
+
+    @R0
+    D = M
+
+    @product		// add R0 to 'product'
+    M = D + M
+
+    @i			// reduce i
+    M = M - 1	
+
+    @LOOP		// restart the loop
+    0;JMP
+
+(END)
+    @product		// load 'product' to D
+    D = M
+  
+    @R2			// store result in R2
+    M = D
+
+    @END
+    0;JMP 		// infinite loop to prevent hacking
