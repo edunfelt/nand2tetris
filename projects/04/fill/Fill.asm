250fa18f7d631d534a78ff0e9164a506a671ec93
Emilia Dunfelt <emilia@dunfelt.se>
2019-12-14 17:54:43 +0100

    I/O-Handling program Fill commit

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,56 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(START)		// starting conditions, set address to first pixel and reset pixel-counter to 8192
+    @SCREEN
+    D = A
+    @addr
+    M = D
+
+    @8192	// total number of pixels to be "painted" (256 * 32)
+    D = A
+    @pixels
+    M = D
+
+(CHECK) 	// obtain the user input from keyboard
+    @pixels	// check if there are pixels left to paint
+    D = M
+    @START
+    D;JEQ	// if not, goto START
+
+    @KBD	// check keyboard input at RAM[24576]
+    D = M
+
+    @BLACKSC 	// black screen if key is pressed
+    D;JNE
+
+    @BLANKSC 	//blank screen if no key is pressed
+    D;JEQ
+
+    (BLACKSC)	
+	@addr	// set the pixel at addr to 1
+	A = M
+	M = -1
+
+	@addr
+	M = M + 1
+
+	@pixels	// one less pixel to paint
+	M = M - 1
+	
+	@CHECK	// retrieve user input
+	0;JMP
+
+    (BLANKSC)	
+	@addr	// set the pixel at addr to 0
+	A = M
+	M = 0
+
+	@addr
+	M = M + 1
+
+	@pixels
+	M = M - 1
+	
+	@CHECK
+	0;JMP
